3330de5d1a1bee4da78b14fd1cbe5135c2be0d35
justi <justi@192.168.254.3>
2018-07-08 20:50:07 -0400

    Boolean demonstration in C++

---
+++ Boolean/Boolean.cpp
@@ -0,0 +1,17 @@
+/*
+ * Boolean.cpp
+ *
+ *  Created on: Jul 8, 2018
+ *      Author: justi
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Hello Boolean" << endl;
+
+}
+
